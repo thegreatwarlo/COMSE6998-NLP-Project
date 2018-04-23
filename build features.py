ede7989ad095dc7866199e8231fa8337f4253ee6
Carlo <carloprovinciali@gmail.com>
2018-04-23 08:10:02 -0400

    added sample features

--- build features.py
+++ build features.py
@@ -13,7 +13,7 @@ import numpy as np
 #from collections import defaultdict
 
 
-IN_FILE = './data/bitcoin_markets_daily_discussion_03172018_04082018.csv'
+IN_FILE = './data/data/bitcoin_markets_daily_discussion_september_v1.csv'
 LEXICON = './combined.txt'
 
 
@@ -37,7 +37,7 @@ with open(IN_FILE) as f:
     
 def sentiment_normal_avg_byday(df, lexicon_name):
     # lexicon name can only be "vader", "socialsent".
-    sent_catogory = ["compound", "neutral", "negative", "positive"]
+    sent_catogory = ["compound", "neu", "neg", "pos"]
     col_names = [lexicon_name+"_"+i for i in sent_catogory]
     groupby_dict = {}
     for key in col_names:
@@ -47,7 +47,7 @@ def sentiment_normal_avg_byday(df, lexicon_name):
 def sentiment_votes_avg_byday(df, lexicon_name):
     weighted_mean = lambda x: np.average(x, weights=df.loc[x.index, "upvotes"])
     # lexicon name can only be "vader", "socialsent".
-    sent_catogory = ["compound", "neutral", "negative", "positive"]
+    sent_catogory = ["compound", "neu", "neg", "pos"]
     col_names = [lexicon_name+"_"+i for i in sent_catogory]
     renames = [lexicon_name+"_"+"votes"+"_"+i for i in sent_catogory]
     groupby_dict = {}
@@ -60,7 +60,7 @@ def sentiment_votes_avg_byday(df, lexicon_name):
 def sentiment_child_avg_byday(df, lexicon_name):
     weighted_mean = lambda x: np.average(x, weights=df.loc[x.index, "num_child"])
     # lexicon name can only be "vader", "socialsent".
-    sent_catogory = ["compound", "neutral", "negative", "positive"]
+    sent_catogory = ["compound", "neu", "neg", "pos"]
     col_names = [lexicon_name+"_"+i for i in sent_catogory]
     renames = [lexicon_name+"_"+"child"+"_"+i for i in sent_catogory]
     groupby_dict = {}
@@ -89,19 +89,62 @@ def opinion_avg_byday(df):
 # main function
 
 data_test = pd.DataFrame(corpus)
-data_test.columns = ["comment_id","date","body","parent_id","vader_scores","socialsent_scores"]
+data_test.columns = ["comment_id","daily_discussion_date","created","body",
+                     "parent_id","vader_scores","upvotes","downvotes",
+                     "author_opinion","socialsent_scores"]
+
+data_test = pd.concat([data_test.drop(['socialsent_scores'], axis=1), 
+                       data_test['socialsent_scores'].apply(pd.Series)], axis=1)
+
+data_test = data_test.rename(index = str, columns = 
+                 {"pos": "socialsent_pos", "neg": "socialsent_neg", 
+                  "neu": "socialsent_neu", "compound":"socialsent_compound"})
 
 for index, row in data_test.iterrows():
-    #convert vader score to dict
-    row['vader_scores'] = eval(row['vader_scores'])
+    #convert vader score to entries
+    data_test.set_value(index, 'vader_scores', eval(row['vader_scores']))
+    
     #parse date
-    Month = row['date'].split(',')[1].split(' ')[1][0:3]
-    Day = row['date'].split(',')[1].split(' ')[2]
-    year = row['date'].split(',')[2].split(' ')[-1]
-    row['date'] = datetime.strptime(Month + ' ' + Day + ' ' + year, '%b %d %Y')
+    dt = datetime.fromtimestamp(int(row['created'].split('.')[0])).strftime('%Y-%m-%d %H')
+    data_test.set_value(index, 'daily_discussion_date', dt)
+    
+data_test = pd.concat([data_test.drop(['vader_scores'], axis=1), 
+                       data_test['vader_scores'].apply(pd.Series)], axis=1)
+    
+data_test = data_test.rename(index = str, columns = 
+                 {"pos": "vader_pos", "neg": "vader_neg", 
+                  "neu": "vader_neu", "compound":"vader_compound"})
     
 # get the number of next-layer comments
-data_test["num_child"] = data_test["comment_id"].apply(lambda x: data_test[data_test["parent_id"]==x].shape[0])
+data_test["num_child"] = data_test["comment_id"].apply(
+        lambda x: data_test[data_test["parent_id"]==x].shape[0])
+
+# sentiment features:
+print("extracting sentiment features....")
+daily_feature = sentiment_normal_avg_byday(data_test, "vader")
+daily_feature = pd.concat([daily_feature, sentiment_normal_avg_byday(data_test, "socialsent")], axis = 1)
+#daily_feature = pd.concat([daily_feature, sentiment_votes_avg_byday(data_test, "vader")], axis=1)
+#daily_feature = pd.concat([daily_feature, sentiment_votes_avg_byday(data_test, "socialsent")], axis=1)
+# get the number of next-layer comments
+#daily_feature = pd.concat([daily_feature, sentiment_child_avg_byday(data_test, "vader")], axis=1)
+
+# statistical features:
+print("extracting statistical features....")
+data_test = get_author_opinion(data_test)
+
+df_num_comments = data_test.groupby("daily_discussion_date").size().to_frame()
+df_num_comments.columns = ["num_comments"]
+
+df_num_children = data_test.groupby("daily_discussion_date").agg({"num_child":"mean"})
+
+data_test['num_word'] = [len(x["body"]) for i,x in data_test.iterrows()]
+df_num_word = data_test.groupby("daily_discussion_date").agg({"num_word":"mean"})
+
+daily_feature = pd.concat([daily_feature, opinion_avg_byday(data_test), 
+                           df_num_comments, df_num_children, df_num_word], axis=1)
+
+daily_feature.to_csv('features_test.csv')
+
 
 
 
